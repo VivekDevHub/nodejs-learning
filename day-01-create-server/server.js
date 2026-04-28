0d42b7582c09fe76a570034d35f77f39d0405a3e
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-04-29 00:39:53 +0530

    day01-server-creation

--- day-01-create-server/server.js
+++ day-01-create-server/server.js
@@ -1,11 +1,9 @@
-let http = require('http');
+let http = require("http");
 let server = http.createServer((req, res) => {
- res.end("Hey are love Apple ...")
- console.log('=*=*=**==*=* after hit request on browser');
- 
-})
+  res.end("Hey are love Apple ...");
+  console.log("=*=*=**==*=* after hit request on browser");
+});
 
-server.listen(3000,() => {
-    console.log("server is running on port 3000");
-    
-})
+server.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
