91ee8bcb35f1ae68cee06d088b771e508da85b49
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-03 23:40:14 +0530

    Task-Product-Crud-API

---
+++ day-05-task-product-crud-api/src/config/db.js
@@ -0,0 +1,13 @@
+// const mongoose = require("mongoose");
+// //mongodb all operations returns promises
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect('mongodb://localhost:27017/productDB');
+//     console.log("Mongo DB Connected");
+//   } catch (error) {
+//     console.log("error in connecting DB", error);
+//   }
+// };
+
+// module.exports = connectDB
