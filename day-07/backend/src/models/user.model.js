184a02c04afd817901ba878f6a479dac7eb90bbb
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-05 01:26:14 +0530

    day07-folderStructure

---
+++ day-07/backend/src/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+    },
+
+    mobile: {
+      type: Number,
+      required: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const UserModel = mongoose.model("users", userSchema);
