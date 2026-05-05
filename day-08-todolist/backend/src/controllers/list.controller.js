a3344f5af01163c66a818b1ba9ea74fea128d0c5
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-06 01:09:37 +0530

    To-Do-List

---
+++ day-08-todolist/backend/src/controllers/list.controller.js
@@ -0,0 +1,117 @@
+const ListModel = require("../models/list.model");
+
+const createListController = async (req, res) => {
+  try {
+    let { name, description } = req.body;
+
+    if (!name || !description) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    let newList = await ListModel.create({
+      taskName: name,
+      description,
+    });
+
+    return res.status(201).json({
+      message: "List Created Successfully",
+      list: newList,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+const getAllListsController = async (req, res) => {
+  try {
+    let allLists = await ListModel.find();
+
+    if (!allLists.length) {
+      return res.status(204).json({
+        message: "List fetched successfully",
+        lists: allLists,
+      });
+    }
+
+    return res.status(200).json({
+      message: "List Fetched Successfully",
+      lists: allLists,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+const updateListController = async (req, res) => {
+  try {
+    let listId = req.params.id;
+    if (!listId) {
+      return res.status(400).json({
+        message: "Id not found",
+      });
+    }
+
+    let { name, description } = req.body;
+
+    if (!name || !description) {
+      return res.status(400).json({
+        message: "All fields required",
+      });
+    }
+
+    let updatedList = await ListModel.findByIdAndUpdate(
+      listId,
+      {
+        taskName: name,
+        description,
+      },
+      {
+        new: true,
+      },
+    );
+
+    return res.status(200).json({
+      message: "List updated successfully",
+      list: updatedList,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+const deleteListController = async (req, res) => {
+  try {
+    let listId = req.params.id;
+
+    if (!listId) {
+      return res.status(404).json({
+        message: "Id not found",
+      });
+    }
+
+    await ListModel.findByIdAndDelete(listId);
+
+    return res.status(200).json({
+      message: "list deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = {
+  createListController,
+  getAllListsController,
+  updateListController,
+  deleteListController,
+};
