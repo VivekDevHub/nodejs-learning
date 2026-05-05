62caa0a26b75ae7cfec9003726b5f328aa86ab5a
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-06 01:09:43 +0530

    To-Do-List

---
+++ day-08-todolist/backend/src/routes/list.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  createListController,
+  getAllListsController,
+  updateListController,
+  deleteListController,
+} = require("../controllers/list.controller");
+
+const router = express.Router();
+
+router.post("/create", createListController);
+router.get("/", getAllListsController);
+router.put("/update/:id", updateListController);
+router.delete("/delete/:id", deleteListController);
+
+module.exports = router;
