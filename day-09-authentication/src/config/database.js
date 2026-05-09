d57341bf419bb1b9d408cca8574587dbd685e513
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-09 16:47:10 +0530

    auth register flow

---
+++ day-09-authentication/src/config/database.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose");
+
+let connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("error in connecting db ...", error);
+  }
+};
+
+module.exports = connectDB
