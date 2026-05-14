6fdfe0e10819e992a2ff9cb294df282d854c2e03
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-15 00:39:34 +0530

    authentication

--- day-09-authentication/src/app.js
+++ day-09-authentication/src/app.js
@@ -1,14 +1,13 @@
-let express = require("express")
+let express = require("express");
 let authRoutes = require("./routes/auth.routes");
 let cookieParser = require("cookie-parser");
-
+let postRoutes = require("./routes/post.routes");
 let app = express();
 
 app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
 module.exports = app;
-
-
