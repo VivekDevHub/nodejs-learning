a0c5dd3dcd8e6358b5ce35ec6fb0dde4e7d3028f
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-15 01:02:01 +0530

    Access-Refresh-Token

---
+++ day-12-ART/src/routes/auth.routes.js
@@ -0,0 +1,15 @@
+let express = require("express");
+const {
+  registerController,
+  loginController,
+  getRefreshTokenController,
+} = require("../controllers/auth.controller");
+
+let router = express.Router();
+
+router.get("/getRefreshToken", getRefreshTokenController);
+
+router.post("/register", registerController);
+router.post("/login", loginController);
+
+module.exports = router;
