9f1f87071016b2b50036a5175366df1fed7370a8
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-14 23:56:01 +0530

    auth middleware

--- day-09-authentication/src/routes/auth.routes.js
+++ day-09-authentication/src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 let express = require("express");
-const {registerController} = require("../controllers/auth.controller")
+const {registerController, loginController} = require("../controllers/auth.controller")
 
 let router = express.Router();
 
 router.post("/register", registerController);
+router.post("/login", loginController);
 
 module.exports = router;
