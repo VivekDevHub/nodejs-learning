28e1d399f0a8fd049dced1bdbf173a6076dbb49d
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-15 01:01:49 +0530

    Access-Refresh-Token

---
+++ day-12-ART/src/app.js
@@ -0,0 +1,13 @@
+let express = require("express");
+let authRoutes = require("./routes/auth.routes");
+let homeRoutes = require("./routes/home.routes");
+let cookieParser = require("cookie-parser");
+let app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/home", homeRoutes);
+
+module.exports = app;
