6b2a323b97edd7e1af8a933dc1588f956d400fd5
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-19 23:43:27 +0530

    global-error-handler

--- day-13-global-error-handler/src/utils/asyncHandler.js
+++ day-13-global-error-handler/src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 let asyncHandler = (requestHandler) => {
   return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((error) =>
-      next(error)
+      next(error),
     );
   };
 };
