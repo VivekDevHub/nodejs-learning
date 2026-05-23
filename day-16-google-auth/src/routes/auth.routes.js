bf4db63b45d2214b3c90a29829eed6d6a0749fb9
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-23 10:08:23 +0530

    google-auth

---
+++ day-16-google-auth/src/routes/auth.routes.js
@@ -0,0 +1,28 @@
+let express = require("express");
+const passport = require("passport");
+let jwt = require("jsonwebtoken");
+
+let router = express.Router();
+
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    session: false,
+  })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/", session: false }),
+  (req, res) => {
+    let token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, {
+      expiresIn: "15m",
+    });
+    res.cookie("token", token);
+    
+    return res.send("ok");
+  }
+);
+
+module.exports = router;
