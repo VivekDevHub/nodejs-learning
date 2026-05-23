aaf8e99effd46723a6b646a09432c5d862b6a61c
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-23 10:19:20 +0530

    file-upload

---
+++ day-17-file-upload/backend/src/routes/file.route.js
@@ -0,0 +1,17 @@
+let express = require("express");
+const {
+  imageUploadController,
+  multipleUploadController,
+} = require("../controllers/file.controller");
+const upload = require("../config/multer");
+
+let router = express.Router();
+
+router.post("/image-upload", upload.single("image"), imageUploadController);
+router.post(
+  "/multi-uploads",
+  upload.array("images", 5),
+  multipleUploadController
+);
+
+module.exports = router;
