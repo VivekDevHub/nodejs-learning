5308370c8fab632f3c58dd003c8be432101ab6ad
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-23 10:25:53 +0530

    nodemailer

---
+++ day-18-nodemailer/src/app.js
@@ -0,0 +1,16 @@
+let express = require("express");
+const sendEmails = require("./config/mail.service");
+
+let app = express();
+
+app.get("/sendmail", async (req, res) => {
+  await sendEmails(
+    "vivekushwah357@gmail.com",
+    "You are very consistent person",
+    "mujhe pta hai tum kar sakte ho..., kuki....... assaaaann hai..."
+  );
+
+  return res.send("gayaaa tata goodbye ");
+});
+
+module.exports = app;
