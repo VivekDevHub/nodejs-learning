9d19d1b282e4ee6ec9ada1406476ca775919bfba
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-24 20:48:13 +0530

    nodemailer

--- day-18-nodemailer/src/app.js
+++ day-18-nodemailer/src/app.js
@@ -7,7 +7,7 @@ app.get("/sendmail", async (req, res) => {
   await sendEmails(
     "vivekushwah357@gmail.com",
     "You are very consistent person",
-    "mujhe pta hai tum kar sakte ho..., kuki....... assaaaann hai..."
+    "mujhe pta hai tum kar sakte ho..., kuki....... assaaaann hai...",
   );
 
   return res.send("gayaaa tata goodbye ");
