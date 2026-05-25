3945f0c4563d487c31b68ab7c097ac43323a46ab
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-25 22:46:18 +0530

    logout with token blacklisting

---
+++ day-19-20-forgot-pass/src/utils/generateToken.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+};
+
+const generateRawToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.RAW_SECRET, {
+    expiresIn: "15m",
+  });
+};
+
+module.exports = {
+  generateToken,
+  generateRawToken,
+};
