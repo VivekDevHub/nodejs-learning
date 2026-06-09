b94b1307217b535aca16cc56dde18aaf3a647610
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-06-09 23:54:48 +0530

    vote

--- day-25-vote/server.js
+++ day-25-vote/server.js
@@ -1,12 +1,11 @@
-import app from './src/app.js';
-import initSocket from './src/sockets/socket.server.js';
-import { createServer } from 'http';
-
+import app from "./src/app.js";
+import initSocket from "./src/sockets/socket.server.js";
+import { createServer } from "http";
 
 const httpServer = createServer(app);
 
 initSocket(httpServer);
 
 httpServer.listen(3000, () => {
-    console.log(`Server is running on port http://localhost:3000`);
-})
+  console.log(`Server is running on port http://localhost:3000`);
+});
