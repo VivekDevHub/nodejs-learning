645f5c361bdf363d13e4a071d2851656b2a71402
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-06-09 23:54:56 +0530

    vote

--- day-25-vote/src/sockets/socket.server.js
+++ day-25-vote/src/sockets/socket.server.js
@@ -1,49 +1,43 @@
-import { Server } from 'socket.io';
+import { Server } from "socket.io";
 
-const voteCounts = {}
+const voteCounts = {};
 const sockets = [];
 
 export default function initSocket(httpServer) {
+  const io = new Server(httpServer);
 
-    const io = new Server(httpServer);
+  io.on("connection", (socket) => {
+    const { room } = socket.handshake.query;
+    console.log(`User joined room: ${room}`);
+    socket.join(room);
 
-    io.on('connection', (socket) => {
+    if (!voteCounts[room]) {
+      voteCounts[room] = { yes: 0, no: 0 };
+    }
 
-        const { room } = socket.handshake.query;
-        console.log(`User joined room: ${room}`);
-        socket.join(room);
+    socket.on("vote_yes", () => {
+      if (sockets.includes(socket.id)) {
+        return;
+      }
 
-        if (!voteCounts[ room ]) {
-            voteCounts[ room ] = { yes: 0, no: 0 };
-        }
+      voteCounts[room].yes += 1;
+      io.to(room).emit("vote_update", voteCounts[room]);
 
-
-        socket.on("vote_yes", () => {
-
-            if (sockets.includes(socket.id)) {
-                return;
-            }
-
-            voteCounts[ room ].yes += 1;
-            io.to(room).emit("vote_update", voteCounts[ room ]);
-
-            sockets.push(socket.id);
-        })
-
-        socket.on("vote_no", () => {
-
-            if (sockets.includes(socket.id)) {
-                return;
-            }
-
-            voteCounts[ room ].no += 1;
-            io.to(room).emit("vote_update", voteCounts[ room ]);
-            sockets.push(socket.id);
-        })
-
-        socket.on('disconnect', () => {
-            console.log('A user disconnected');
-        });
+      sockets.push(socket.id);
     });
 
+    socket.on("vote_no", () => {
+      if (sockets.includes(socket.id)) {
+        return;
+      }
+
+      voteCounts[room].no += 1;
+      io.to(room).emit("vote_update", voteCounts[room]);
+      sockets.push(socket.id);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("A user disconnected");
+    });
+  });
 }
